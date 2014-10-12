ef0de252456cbc08240bb6ff95a74b288e0e7c92
Michael Buckley <codefisher@users.noreply.github.com>
2014-10-12 20:53:26 +0000

    fixing url miss conf

--- downloads/urls.py
+++ downloads/urls.py
@@ -3,5 +3,5 @@ from codefisher_apps.downloads import views
 
 urlpatterns = patterns('',
     url(r'^(?P<path>.*)/version/(?P<version>.*)$', views.release_notes, name='download-release-notes'),
-    url(r'^download/(?P<path>.*)$', views.file_listing, name='download-file-listing'),
+    url(r'^(?P<path>.*)$', views.file_listing, name='download-file-listing'),
 )
